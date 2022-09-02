4e06d2d09933033add7535fe4d67248eea625086
natani51 <natnaelendale51@gmail.com>
2022-09-02 14:02:56 +0000

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-
-def best_score(my_dict):
-    if my_dict:
-        return (max(my_dict))
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    biggest = 0
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+    for key, value in a_dictionary.items():
+        if value == biggest:
+            return key
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
+
 def multiply_by_2(a_dictionary):
-    temp = dict(a_dictionary)
-    for key, value in temp.items():
-        temp[key] = value * 2
-    return 
+    new_dic = {}
+    for keys in a_dictionary:
+        new_dic[keys] = a_dictionary[keys] * 2
+    return new_dic
