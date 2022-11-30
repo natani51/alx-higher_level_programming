43066456675c499c388693871eb4ad7e2b28a356
Natnael Endale <62070546+natani51@users.noreply.github.com>
2022-11-30 11:15:00 -0800

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
+const dict = require('./101-data.js').dict;
 const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+for (const value of Object.values(dict)) {
+  const newList = [];
+  newDict[value] = newList;
+  for (const k in dict) {
+    if (dict[k] === Number(value)) {
+      newList.push(k);
+    }
   }
 }
 console.log(newDict);
