57f580a3d8ee2106c8c054573d97f6467ac05760
natani51 <natnaelendale51@gmail.com>
2022-12-01 18:13:55 +0000

    6-model_state.sql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -1,6 +1,6 @@
--- Create states table in hbtn_0e_101_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_101_usa;
-USE hbtn_0e_101_usa;
+-- Create states table in hbtn_0e_4_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
+USE hbtn_0e_4_usa;
 CREATE TABLE IF NOT EXISTS states ( 
     id INT NOT NULL AUTO_INCREMENT, 
     name VARCHAR(256) NOT NULL,
