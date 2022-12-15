0973ad6b2ca317599bd587e0fa9dbe4519a0e913
natani51 <natnaelendale51@gmail.com>
2022-12-15 09:37:06 -0800

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,72 +1,16 @@
 #!/usr/bin/python3
-"""Write a function that finds a peak in a list of unsorted integers."""
-
-
-def peak_rec(arr, l, r):
-    """Recursive way"""
-    if l == r:
-        return arr[l]
-    if r - l == 1:
-        return max(arr[r], arr[l])
-    m = (r + l) // 2
-    if arr[m] < arr[m + 1]:
-        return peak_rec(arr, m + 1, r)
-    return peak_rec(arr, l, m)
-
-
-def find_peak_rec(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    l = len(list_of_integers)
-    arr = list_of_integers
-    return peak_rec(arr, 0, l - 1)
-
-
-def find_peak_0(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    l = len(list_of_integers)
-    m = l // 2
-    arr = list_of_integers
-    while True:
-        if m != 0 and m != l - 1 and arr[m - 1] < arr[m] < arr[m + 1]:
-            m = (l + m) // 2
-        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] > arr[m + 1]:
-            m = m // 2
-        elif m != 0 and m != l - 1 and arr[m - 1] > arr[m] < arr[m + 1]:
-            m = m // 2
-        else:
-            return arr[m]
+"""contains the function find_peak"""
 
 
 def find_peak(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    l = len(list_of_integers)
+    """finds a peak in a list of unsorted integers"""
+    li = list_of_integers
+    l = len(li)
+    if l == 0:
+        return
     m = l // 2
-    arr = list_of_integers
-    while True:
-        if m in (0, l - 1) or arr[m - 1] < arr[m] > arr[m + 1]:
-            return arr[m]
-        if arr[m] < arr[m + 1]:
-            m = (l + m) // 2
-        else:
-            m = m // 2
-
-
-def find_peak_2(list_of_integers):
-    """Function to find a peak in a list"""
-    if list_of_integers == []:
-        return None
-    p = 0
-    arr = list_of_integers
-    while True:
-        try:
-            if arr[p] > arr[p + 1]:
-                return arr[p]
-            p += 1
-        except IndexError:
-            return arr[p]
+    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+        return li[m]
+    if m != l - 1 and li[m + 1] > li[m]:
+        return find_peak(li[m + 1:])
+    return find_peak(li[:m])
